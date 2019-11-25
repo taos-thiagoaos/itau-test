4c32f130218f6cc7552e1c777118bf73c194f4bd
Thiago Antonius Oliveira Souza <thiagoaos@gmail.com>
2019-11-25 11:49:48 -0300

    Updates

---
+++ precache-manifest.5ae5bc45875edadd43245b23abfb6726.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c579554288080a300171a31c794c30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de5c243a2a3bc606a93e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f3709df2efbcb8049fd5",
+    "url": "/static/js/2.ed1d6ff1.chunk.js"
+  },
+  {
+    "revision": "de5c243a2a3bc606a93e",
+    "url": "/static/js/main.929b404c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/itau-test/precache-manifest.538b61442cd5730d1f04289f2659fbc7.js"
+  "/precache-manifest.5ae5bc45875edadd43245b23abfb6726.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/itau-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
