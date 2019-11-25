3e18c55198f3a9c0f4f582516f10d26b142ed6ba
Thiago Antonius Oliveira Souza <thiagoaos@gmail.com>
2019-11-25 11:29:43 -0300

    Updates

---
+++ precache-manifest.ebf450e0a559735ed6167e057db7b462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b4cebfad7f52715e332fea51336926",
+    "url": "/itau-test/index.html"
+  },
+  {
+    "revision": "40ef5b4dbf484f5f25be",
+    "url": "/itau-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f3709df2efbcb8049fd5",
+    "url": "/itau-test/static/js/2.ed1d6ff1.chunk.js"
+  },
+  {
+    "revision": "40ef5b4dbf484f5f25be",
+    "url": "/itau-test/static/js/main.ed0f8263.chunk.js"
+  },
+  {
+    "revision": "83a16ce404b6e25b6cd0",
+    "url": "/itau-test/static/js/runtime~main.3e9959a8.js"
+  }
+]);
